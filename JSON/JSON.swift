133750c04e4932e9ba68c9612f9594bc35878305
Bei Li <kylin.roc@me.com>
2018-06-15 16:19:53 +0800

    Add enum JSON and literal

--- JSON/JSON.swift
+++ JSON/JSON.swift
@@ -4,3 +4,68 @@
 //
 //  Created by 李孛 on 2018/6/15.
 //
+
+import Foundation
+
+public enum JSON: Equatable {
+    public typealias String = Swift.String
+    public typealias Number = NSNumber
+    public typealias Object = [String: JSON]
+    public typealias Array = [JSON]
+
+    case string(String)
+    case number(Number)
+    case object(Object)
+    case array(Array)
+    case `true`
+    case `false`
+    case null
+}
+
+// MARK: - Literal
+
+extension JSON: ExpressibleByStringLiteral {
+    public init(stringLiteral value: String) {
+        self = .string(value)
+    }
+}
+
+extension JSON: ExpressibleByIntegerLiteral {
+    public init(integerLiteral value: Int) {
+        self = .number(value as Number)
+    }
+}
+
+extension JSON: ExpressibleByFloatLiteral {
+    public init(floatLiteral value: Double) {
+        self = .number(value as Number)
+    }
+}
+
+extension JSON: ExpressibleByDictionaryLiteral {
+    public init(dictionaryLiteral elements: (String, JSON)...) {
+        self = .object(Object(uniqueKeysWithValues: elements))
+    }
+}
+
+extension JSON: ExpressibleByArrayLiteral {
+    public init(arrayLiteral elements: JSON...) {
+        self = .array(elements)
+    }
+}
+
+extension JSON: ExpressibleByBooleanLiteral {
+    public init(booleanLiteral value: Bool) {
+        if value {
+            self = .true
+        } else {
+            self = .false
+        }
+    }
+}
+
+extension JSON: ExpressibleByNilLiteral {
+    public init(nilLiteral: ()) {
+        self = .null
+    }
+}
--- JSONTests/JSONTests.swift
+++ JSONTests/JSONTests.swift
@@ -8,4 +8,14 @@
 import XCTest
 @testable import JSON
 
-class JSONTests: XCTestCase {}
+class JSONTests: XCTestCase {
+    func testLiteral() {
+        XCTAssert(JSON.string("string") == "string")
+        XCTAssert(JSON.number(42) == 42.0)
+        XCTAssert(JSON.object(["key": "value"]) == ["key": "value"])
+        XCTAssert(JSON.array([1, 3, "5", 7.0]) == [1, 3, "5", 7.0])
+        XCTAssert(JSON.true == true)
+        XCTAssert(JSON.false == false)
+        XCTAssert(JSON.null == nil)
+    }
+}
