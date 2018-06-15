c7a883be1cde47b4c50b9a63d7e8e186929a3e53
Bei Li <kylin.roc@me.com>
2018-06-15 10:37:12 +0800

    Update tests

--- JSONTests/JSONTests.swift
+++ JSONTests/JSONTests.swift
@@ -11,39 +11,57 @@ import XCTest
 class JSONTests: XCTestCase {
     func testJSON() {
         var json: JSON
+
+        // string
+        json = "string"
+        XCTAssert(json.string == "string")
+
+        // number
+        json = 42.0
+        XCTAssert(json.number == 42)
+
         // object
         json = ["key": "value"]
         XCTAssert(json.object?["key"]?.string == "value")
         json = ["key": 42.0]
         XCTAssert(json.object?["key"]?.number == 42)
+
         // array
         json = ["value"]
         XCTAssert(json.array?[0].string == "value")
+
         // true
         json = true
         XCTAssert(json.bool == true)
         json = NSNumber(value: true)
         XCTAssert(json.bool == true)
+
         // false
         json = false
         XCTAssert(json.bool == false)
         json = NSNumber(value: false)
         XCTAssert(json.bool == false)
+
         // null
         json = Optional<Bool>.none
         XCTAssert(json.isNull)
         json = NSNull()
         XCTAssert(json.isNull)
-        // string
-        json = "string"
-        XCTAssert(json.string == "string")
-        // number
-        json = 42.0
-        XCTAssert(json.number == 42)
     }
 
     func testAnyJSON() {
         var json: AnyJSON
+
+        // string
+        json = "string"
+        XCTAssert(json.string == "string")
+
+        // number
+        json = 42
+        XCTAssert(json.number == 42.0)
+        json = 0.01
+        XCTAssert(json.number == 0.01)
+
         // object
         json = ["null": nil, "true": true, "number": 42]
         XCTAssert(json.object?["null"]?.isNull == true)
@@ -58,34 +76,30 @@ class JSONTests: XCTestCase {
         XCTAssert(json.object?["object"]?.object?["string"]?.string == "string")
         XCTAssert(json.object?["array"]?.array?[0].number == 1.0)
         XCTAssert(json.object?["array"]?.array?[1].isNull == true)
+
         // array
         json = ["null", nil, true, 42]
         XCTAssert(json.array?[0].isNull == false)
         XCTAssert(json.array?[1].isNull == true)
         XCTAssert(json.array?[2].bool == true)
         XCTAssert(json.array?[3].number == 42)
+
         // true
         json = true
         XCTAssert(json.bool == true)
         json = AnyJSON(NSNumber(value: true))
         XCTAssert(json.bool == true)
+
         // false
         json = false
         XCTAssert(json.bool == false)
         json = AnyJSON(NSNumber(value: false))
         XCTAssert(json.bool == false)
+
         // null
         json = nil
         XCTAssert(json.isNull == true)
         json = AnyJSON(NSNull())
         XCTAssert(json.isNull == true)
-        // string
-        json = "string"
-        XCTAssert(json.string == "string")
-        // number
-        json = 42
-        XCTAssert(json.number == 42.0)
-        json = 0.01
-        XCTAssert(json.number == 0.01)
     }
 }
