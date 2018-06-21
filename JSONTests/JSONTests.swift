5062ae590d2c7ef6c0844ecec86ea1a72a3802b9
Bei Li <kylin.roc@me.com>
2018-06-21 13:05:26 +0800

    Add tests for JSON

--- JSONTests/JSONTests.swift
+++ JSONTests/JSONTests.swift
@@ -62,4 +62,47 @@ class JSONTests: XCTestCase {
         XCTAssertEqual(JSON(array as Any), .array(array))
         XCTAssertEqual(JSON(NSNull() as Any), .null)
     }
+
+    func testLiterals() {
+        XCTAssertEqual(JSON.string("string"), "string")
+        XCTAssertEqual(JSON.number(1), 1)
+        XCTAssertEqual(JSON.number(1), 1.0)
+        XCTAssertEqual(JSON.object(["key": .null]), ["key": nil])
+        XCTAssertEqual(JSON.array([.true, .null, .false]), [true, nil, false])
+    }
+
+    func testProperties() {
+        var json: JSON
+
+        let string = "string"
+        json = .string(string)
+        XCTAssertEqual(json.string, string)
+
+        let number: NSNumber = 42
+        json = .number(number)
+        XCTAssertEqual(json.number, number)
+        XCTAssertNil(json.string)
+
+        let object: [String: JSON] = ["key": nil, "x": 42]
+        json = .object(object)
+        XCTAssertEqual(json.object, object)
+        XCTAssertNil(json.number)
+
+        let array: [JSON] = ["key", nil, 42, [nil]]
+        json = .array(array)
+        XCTAssertEqual(json.array, array)
+        XCTAssertNil(json.object)
+
+        json = .true
+        XCTAssertEqual(json.bool, true)
+        XCTAssertNil(json.array)
+
+        json = .false
+        XCTAssertEqual(json.bool, false)
+        XCTAssertFalse(json.isNull)
+
+        json = .null
+        XCTAssertTrue(json.isNull)
+        XCTAssertNil(json.bool)
+    }
 }
