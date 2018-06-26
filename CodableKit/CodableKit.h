64bdca31e9564dfdfe756f1f55b07d19371c6d1e
Bei Li <kylin.roc@me.com>
2018-06-26 23:39:11 +0800

    Add project

---
+++ CodableKit/CodableKit.h
@@ -0,0 +1,18 @@
+//
+//  CodableKit.h
+//  CodableKit
+//
+//  Created by 李孛 on 2018/6/26.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CodableKit.
+FOUNDATION_EXPORT double CodableKitVersionNumber;
+
+//! Project version string for CodableKit.
+FOUNDATION_EXPORT const unsigned char CodableKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodableKit/PublicHeader.h>
+
+
