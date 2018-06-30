79037a943881023ed9bc2be64bd0e6426f513cce
Bei Li <kylin.roc@me.com>
2018-07-01 00:34:36 +0800

    Update tests

--- Tests/CodableKitTests/CodableKitTests.swift
+++ Tests/CodableKitTests/CodableKitTests.swift
@@ -9,41 +9,59 @@ import XCTest
 @testable import CodableKit
 
 final class CodableKitTests: XCTestCase {
-    let data = """
-        {
-            "id": "0",
-            "nested": {
-                "string": "string"
+    func testString() {
+        let tests = [
+            ("", ""),
+            ("property", "property"),
+            ("abcDefGhi", "abc_def_ghi"),
+            ("abcHTTPNice", "abc_http_nice"),
+            ("abc_def_ghi", "abc_def_ghi"),
+        ]
+        for (input, output) in tests {
+            XCTAssertEqual(input.snakeCased(), output)
+        }
+    }
+
+    func testSnakeCasedCodingKey() {
+        struct Model: Codable {
+            let camelCasedProperty: String
+            enum CodingKeys: String, SnakeCasedCodingKey {
+                case camelCasedProperty
             }
         }
-        """
-        .data(using: .utf8)!
+
+        let string = """
+            {"camel_cased_property":"CodableKit"}
+            """
+
+        let data = string.data(using: .utf8)!
+        let model = try! JSONDecoder().decode(Model.self, from: data)
+        XCTAssertEqual(model.camelCasedProperty, "CodableKit")
+
+        do {
+            let data = try! JSONEncoder().encode(model)
+            XCTAssertEqual(String(data: data, encoding: .utf8), string)
+        }
+    }
 
     func testKeyedDecodingContainerProtocol() {
         struct Model: Decodable {
-            let id: String
-            let data: String?
-            let index: Int
-
-            enum CodingKeys: CodingKey {
-                case id
-                case data
-                case index
-            }
-
+            enum CodingKeys: CodingKey { case a, b, c }
             init(from decoder: Decoder) throws {
                 let container = try decoder.container(keyedBy: CodingKeys.self)
-                id = try container.decode(.id)
-                data = try container.decodeIfPresent(.data)
-                index = container[.index] ?? 0
+                let _: Bool = try container.decode(.a)
+                let _: Bool? = try container.decodeIfPresent(.b)
+                let _: Bool? = container[.c]
             }
         }
-
-        let model = try! JSONDecoder().decode(Model.self, from: data)
-
-        XCTAssertEqual(model.id, "0")
-        XCTAssertNil(model.data)
-        XCTAssertEqual(model.index, 0)
+        let data = """
+            {
+                "a": true,
+                "b": null
+            }
+            """
+            .data(using: .utf8)!
+        XCTAssertNotNil(try? JSONDecoder().decode(Model.self, from: data))
     }
 
     func testAnyCodingKey() {
@@ -52,38 +70,50 @@ final class CodableKitTests: XCTestCase {
             XCTAssertEqual(key.stringValue, "key")
             XCTAssertNil(key.intValue)
         }
+
         do {
             let key = AnyCodingKey(intValue: 0)
             XCTAssertEqual(key.stringValue, "0")
             XCTAssertEqual(key.intValue, 0)
         }
+
+        do {
+            let key = AnyCodingKey(index: 0)
+            XCTAssertEqual(key.stringValue, "Index 0")
+            XCTAssertEqual(key.intValue, 0)
+        }
+
         do {
             let key: AnyCodingKey = "key"
             XCTAssertEqual(key.stringValue, "key")
             XCTAssertNil(key.intValue)
         }
 
-        struct Model: Decodable {
-            let id: String
-            let data: String?
-            let index: Int
-            let nestedString: String
-
+        struct Model: Codable {
             init(from decoder: Decoder) throws {
                 let container = try decoder.container()
-                id = try container.decode("id")
-                data = try container.decodeIfPresent("data")
-                index = container["index"] ?? 0
-                let nestedContainer = try container.nestedContainer(forKey: "nested")
-                nestedString = try nestedContainer.decode("string")
+                _ = try container.nestedContainer(forKey: "nested_container")
+                var nestedUnkeyedContainer = try container.nestedUnkeyedContainer(forKey: "nested_unkeyed_container")
+                _ = try nestedUnkeyedContainer.nestedContainer()
+            }
+
+            func encode(to encoder: Encoder) throws {
+                var container = encoder.container()
+                _ = container.nestedContainer(forKey: "nested_container")
+                var nestedUnkeyedContainer = container.nestedUnkeyedContainer(forKey: "nested_unkeyed_container")
+                _ = nestedUnkeyedContainer.nestedContainer()
             }
         }
 
-        let model = try! JSONDecoder().decode(Model.self, from: data)
+        let data = """
+            {
+                "nested_container": {},
+                "nested_unkeyed_container": [{}]
+            }
+            """
+            .data(using: .utf8)!
 
-        XCTAssertEqual(model.id, "0")
-        XCTAssertNil(model.data)
-        XCTAssertEqual(model.index, 0)
-        XCTAssertEqual(model.nestedString, "string")
+        let model = try! JSONDecoder().decode(Model.self, from: data)
+        _ = try! JSONEncoder().encode(model)
     }
 }
