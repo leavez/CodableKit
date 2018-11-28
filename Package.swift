eba9b11409f053a9b74e4500b66cdaf1d9f79b38
Bei Li <kylin.roc@me.com>
2018-11-28 11:17:54 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,12 +1,9 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
     name: "CodableKit",
-    products: [
-        .library(name: "CodableKit", targets: ["CodableKit"]),
-    ],
     targets: [
         .target(name: "CodableKit"),
         .testTarget(name: "CodableKitTests", dependencies: ["CodableKit"]),
