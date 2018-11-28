8c86f816712870660acedb61f7b6a25b4dffbef0
Bei Li <kylin.roc@me.com>
2018-11-28 11:45:02 +0800

    Fix SPM support

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "CodableKit",
+    products: [
+        .library(name: "CodableKit", targets: ["CodableKit"]),
+    ],
     targets: [
         .target(name: "CodableKit"),
         .testTarget(name: "CodableKitTests", dependencies: ["CodableKit"]),
